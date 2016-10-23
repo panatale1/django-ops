6a5ec29d4ae748214812948c3c93d6f57b4e2fcf
panatale1 <panatale1@gmail.com>
2016-10-23 17:21:05 -0400

    extend local_setup for virtualenv

--- local_setup.sh
+++ local_setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+VENV_NAME="django-ops"
+VENV_DIR="$HOME/.virtualenvs"
 
-mkdir -p ~/.virtualenvs
+mkdir -p $VENV_DIR
 
-if [[ $(grep WORKON_HOME ~/.bashrc) -ne 0 ]]; then
+if [[ $(grep WORKON_HOME ~/.bashrc) == "" ]]; then
     echo "export WORKON_HOME=~/.virtualenvs" >> ~/.bashrc
 fi
 
@@ -11,5 +13,11 @@ if [[ wrapper_path != "" ]]; then
     echo $wrapper_path
     if [[ $(grep "source $wrapper_path" ~/.bashrc) == "" ]]; then
         echo "source $wrapper_path" >> ~/.bashrc
+        source "$wrapper_path"
     fi 
 fi
+
+source $VENV_DIR/$VENV_NAME/bin/activate
+#echo $VIRTUAL_ENV
+pip install -r requirements
+
